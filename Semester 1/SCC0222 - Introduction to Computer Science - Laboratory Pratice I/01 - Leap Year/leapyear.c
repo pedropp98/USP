5ded1cf9edbe0d4b274329fbf91b7141789c8fe9
Pedro Perez <pedropc@usp.br>
2021-01-22 22:03:05 -0300

    Adding comments to leapyear.c

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/01 - Leap Year/leapyear.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/01 - Leap Year/leapyear.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Main function
 int main(void){
     int year;
     scanf("%d", &year);
-    if(year % 4 == 0)
+    if(year % 4 == 0) // If the remainder of year by 4 is 0, then year is a leap year
         printf("SIM\n");
     else
         printf("NAO\n");
+    return 0;
 }
