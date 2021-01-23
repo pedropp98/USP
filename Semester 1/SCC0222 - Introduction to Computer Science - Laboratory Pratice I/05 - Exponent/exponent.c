97d3fc309e5d6187ed2f57297d82dbf344fa1e34
Pedro Perez <pedropc@usp.br>
2021-01-22 22:17:43 -0300

    Adding comments to exponent.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/05 - Exponent/exponent.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/05 - Exponent/exponent.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Function definition
 double exponent(double, double);
 
+// Main function
 int main(void){
     double n1, n2;
     scanf("%lf %lf", &n1, &n2);
@@ -10,6 +12,8 @@ int main(void){
     return 0;
 }
 
+//Function declaration
+// Exponent function makes a to the power of b and returns this value
 double exponent(double a, double b){
     int e = 1;
     for(int i = 0; i < b; i++){
