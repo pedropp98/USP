89807362d8a1c1bffaef98e25b94f8ef23ff156b
Pedro Perez <pedropc@usp.br>
2021-01-22 22:19:23 -0300

    Adding comments to real.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/06 - Real Number/real.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/06 - Real Number/real.c
@@ -5,9 +5,10 @@
 int main(void){
     double num, float_part;
     int integer;
-    scanf("%lf", &num);
-    integer = (int)num / 1;
-    float_part = num - integer;
+    scanf("%lf", &num); // Reads a number from stdin
+    integer = (int)num / 1; // Gets its integer part from the division by 1
+    float_part = num - integer; // Gets its floating point part subtracting the integer part from the original number
+    //Prints the result
     printf("%d\n", integer);
     printf("%.4lf\n", float_part);
     printf("%.0lf\n", round(num));
