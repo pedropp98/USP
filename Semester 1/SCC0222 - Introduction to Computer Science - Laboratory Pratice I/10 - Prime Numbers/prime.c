f595af0aaa36c07e064350774132e29b7865d9c2
Pedro Perez <pedropc@usp.br>
2021-01-22 22:25:55 -0300

    Adding comments to prime.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/10 - Prime Numbers/prime.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/10 - Prime Numbers/prime.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Function definition
 void factorization(int);
 
+// Main function
 int main(void){
     int number;
     scanf("%d", &number);
@@ -10,12 +12,14 @@ int main(void){
     return 0;
 }
 
+// Function declaration
+//It factores a number and prints the result
 void factorization(int number){
     for(int i = 2; i <= number; i++){
         if(number % i == 0){
             int count = 0;
             while(number % i == 0){
-                count++;
+                count++; // Adding 1 to count every single time number is divided by i
                 number /= i;
             }
             printf("%d %d\n", i, count);
