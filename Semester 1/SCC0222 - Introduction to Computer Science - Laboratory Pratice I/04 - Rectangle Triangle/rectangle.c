1a78cf299aa4044132bfb7c081ee228388cf3832
Pedro Perez <pedropc@usp.br>
2021-01-22 22:16:30 -0300

    Adding comments to rectangle.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/04 - Rectangle Triangle/rectangle.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/04 - Rectangle Triangle/rectangle.c
@@ -2,14 +2,16 @@
 #include<stdlib.h>
 #include<math.h>
 
+// Function declaration
 void bubblesort(int*);
 int check_sides(int*);
 
+// Main function
 int main(void){
     int *sides = (int*)calloc(3, sizeof(int));
     for(int i = 0; i < 3; i++)
         scanf("%d", &sides[i]);
-    bubblesort(sides);
+    bubblesort(sides); // Calls bubblesort function
     if(check_sides(sides) && pow(sides[0], 2) + pow(sides[1], 2) == pow(sides[2], 2))
         printf("SIM\n");
     else
@@ -18,6 +20,8 @@ int main(void){
     return 0;
 }
 
+//Function definition
+// Bubblesort is a sorting function. It sorts an array in ascending order
 void bubblesort(int *array){
     int copy;
     for(int i = 0; i < 3; i++){
@@ -31,13 +35,10 @@ void bubblesort(int *array){
     }
 }
 
+// It checks if all the sides are different than zero. It returns 1 if do, 0 otherwise
 int check_sides(int *sides){
-    if(sides[0] == 0)
-        return 0;
-    else if(sides[1] == 0)
-        return 0;
-    else if(sides[2] == 0)
-        return 0;
-    else
-        return 1;
+    for(int i = 0; i < 3; i++)
+        if(sides[i] == 0)
+            return 0;
+    return 1;
 }
