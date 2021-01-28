6bd9646d586b2c216e37670df865b9506f662adc
Pedro Perez <pedropc@usp.br>
2021-01-27 23:25:52 -0300

    Adding comments to encrypt.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/11 - Encrypting Words/encrypt.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/11 - Encrypting Words/encrypt.c
@@ -2,26 +2,30 @@
 #include<stdlib.h>
 #include<string.h>
 
+// Defining macros
 #define BUFFER 4096
 #define and &&
 
+// Function definition
 char *readline(FILE*);
 int encrypt_and_find_char_in_string(char, char*);
 
+// Main function
 int main(void){
-    char find_character = fgetc(stdin);
-    getchar();
-    getchar();
-    char *string = readline(stdin);
-    char *copy_string = (char*)malloc((strlen(string)+1)* sizeof(char));
-    strcpy(copy_string, string);
-    int num = encrypt_and_find_char_in_string(find_character, string);
+    char find_character = fgetc(stdin); // Reads the amount of sum to encrypt the string
+    getchar(); // Reads the character 13, all the test case files contain it
+    getchar(); // Reads the break line character
+    char *string = readline(stdin); // Reads the line
+    char *copy_string = (char*)malloc((strlen(string)+1)* sizeof(char)); 
+    strcpy(copy_string, string); // Copies the line read, to keep the original string intact
+    int num = encrypt_and_find_char_in_string(find_character, string); // Encrypts the string
     printf("Numero de trocas: %d\nNova palavra: %s\nPalavra original: %s\n", num, string, copy_string);
     free(string);
     free(copy_string);
     return 0;
 }
 
+// Reads a FILE* and returns a char* containing all the characters in a determined line. It stops reading when it find the break line character or it reaches the end of the file
 char *readline(FILE *in){
     char *string = NULL;
     int pos = 0, character;
@@ -37,6 +41,7 @@ char *readline(FILE *in){
     return string;
 }
 
+// It applies the encrypting algotirthm asked in the exercise, changing all the characters in a determined string, all the character must remain in lower case
 int encrypt_and_find_char_in_string(char character, char *string){
     int found_char = 0;
     for(int i = 0; string[i] != 0; i++){
