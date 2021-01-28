8912d5cbdbbc5f8956fd0e40874c4a2b6a6c42a1
Pedro Perez <pedropc@usp.br>
2021-01-28 00:00:06 -0300

    Adding comments to harmonic-average.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/14 - Harmonic Average/harmonic-average.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/14 - Harmonic Average/harmonic-average.c
@@ -1,35 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Function definition
 int readInt();
 double readDouble();
 double printDouble(double val);
 double calculateAverage(double*, int);
 
-
+// Main function
 int main(void){
     int N_works = readInt();
-    double *grades = (double*)malloc(N_works * sizeof(double));
+    double *grades = (double*)malloc(N_works * sizeof(double)); // Allocates N_works * 8 bytes in heap memory
     for(int i = 0; i < N_works; i++){
         grades[i] = readDouble();
     }
     printDouble(calculateAverage(grades, N_works));
-    free(grades);
+    free(grades); // Deallocates memory allocated for grades
     return 0;
 }
 
+// Reads an integer from stdin and returns it
 int readInt(){
     int number;
     scanf("%d", &number);
     return number;
 }
 
+// Reads a double from stdin and returns it
 double readDouble(){
     double number;
     scanf("%lf", &number);
     return number;
 }
 
+// Calculates the harmonic average using the especific equation in the exercise
 double calculateAverage(double *grades, int n_grades){
     double sum_harmonic_grades = 0;
     for(int i = 0; i < n_grades; i++){
@@ -39,6 +43,7 @@ double calculateAverage(double *grades, int n_grades){
     return average;
 }
 
+// Prints the result
 double printDouble(double val){
     printf("%.2lf", val);
 }
