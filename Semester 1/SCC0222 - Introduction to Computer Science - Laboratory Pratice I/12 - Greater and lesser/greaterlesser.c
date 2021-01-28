cff199e2c83b3b23cd8dac798258f256baa440ea
Pedro Perez <pedropc@usp.br>
2021-01-27 23:50:48 -0300

    Adding comments to greaterlesser.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/12 - Greater and lesser/greaterlesser.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/12 - Greater and lesser/greaterlesser.c
@@ -1,25 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Defining a new type boolean_t
 typedef enum boolean{
     false, true
 }boolean_t;
 
+// Function definition
 void bubblesort(int *array, int size);
 
+// Main function
 int main(void){
     int q_numbers;
     scanf("%d", &q_numbers);
-    int *numbers = (int*)malloc(q_numbers * sizeof(int));
+    int *numbers = (int*)malloc(q_numbers * sizeof(int)); // Allocates q_numbers * 4 bytes in heap memory
     for(int i = 0; i < q_numbers; i++){
         scanf("%d", &numbers[i]);
     }
-    bubblesort(numbers, q_numbers);
-    printf("max: %d\nmin: %d\n", numbers[q_numbers-1], numbers[0]);
-    free(numbers);
+    bubblesort(numbers, q_numbers); // Bubblesort sorts the array numbers
+    printf("max: %d\nmin: %d\n", numbers[q_numbers-1], numbers[0]);  // As numbers is sorted, the lowest number is the first index and the highest is in the last index
+    free(numbers); // Deallocates memory from numbers
     return 0;
 }
 
+// sorts an array containing size items, using the logic of bubblesort algorithm
 void bubblesort(int *array, int size){
     boolean_t sort = true;
     while(sort){
