85086667d1adc98c189061e36591977b46055ae3
Pedro Perez <pedropc@usp.br>
2021-02-05 17:55:11 -0300

    Updating split.c file, there was an error with bubblesort algorithm, implementing my_strtok function and changing a bit string_compare function

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/31 - Split String/split.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/31 - Split String/split.c
@@ -20,18 +20,19 @@ typedef enum{
 
 // Function definition
 char *readline(FILE*);
-words_t *split_word(char*, char);
+words_t *split_word(char*, const char*);
 void erase_words(words_t*);
 void bubblesort(char**, int);
 int string_compare(char*, char*);
 void operate(words_t*, int);
+char *my_strtok(char*, const char*);
 
 // Main function
 int main(void){
     char *input_line = readline(stdin);
     int operation;
     scanf("%d", &operation);
-    words_t *splitted = split_word(input_line, 32);
+    words_t *splitted = split_word(input_line, " ");
     operate(splitted, operation);
     free(input_line);
     erase_words(splitted);
@@ -55,33 +56,22 @@ char *readline(FILE *in){
 }
 
 // Creates a word_t space in heap memory and stores every words from the string separated by the split_character, it counts how many words are found and stores it too into the words_t
-words_t *split_word(char *string, char split_character){
+words_t *split_word(char *string, const char *split_character){
     words_t *split = (words_t*)malloc(sizeof(words_t));
-    split->splitted = (char**)malloc(sizeof(char*));
-    split->splitted[0] = NULL;
     split->q_words = 0;
-    int len = 0;
-    for(int i = 0; string[i] != 0; i++){
-        if(string[i] == split_character){
-            split->q_words++;
-            len = 0;
-            split->splitted = (char**)realloc(split->splitted, (split->q_words+1) * sizeof(char*));
-            split->splitted[split->q_words] = NULL;
-        }
-        else{
-            split->splitted[split->q_words] = (char*)realloc(split->splitted[split->q_words], (len+2) * sizeof(char));
-            split->splitted[split->q_words][len++] = string[i];
-            split->splitted[split->q_words][len] = 0;
-        }
+    split->splitted = (char**)malloc(sizeof(char*));
+    split->splitted[split->q_words++] = my_strtok(string, split_character);
+    
+    while(split->splitted[split->q_words-1] != NULL){
+        split->splitted = (char**)realloc(split->splitted, (split->q_words+1) * sizeof(char*));
+        split->splitted[split->q_words++] = my_strtok(NULL, split_character);
     }
-    split->q_words++;
+    split->q_words--;
     return split;
 }
 
 // Deallocates memory allocated for a word_t type and its members
 void erase_words(words_t *words){
-    for(int i = 0; i < words->q_words; i++)
-        free(words->splitted[i]);
     free(words->splitted);
     free(words);
 }
@@ -89,35 +79,28 @@ void erase_words(words_t *words){
 // Sorts an array of char** containing q_items using the logic of bubblesort
 void bubblesort(char **array, int q_items){
     boolean_t swap = true;
-    int begin = 0;
     while(swap){
         swap = false;      
-        for(int i = begin; i < q_items; i++){
-            if(string_compare(array[begin], array[i]) > 0){
-                int size1 = strlen(array[begin]), size2 = strlen(array[i]);
-                void *data = (void*)malloc(size1+1); // Auxiliar variable to store the char* and not loose it
-                memcpy(data, array[begin], size1+1);
-                array[begin] = (char*)realloc(array[begin], (size2+1) * sizeof(char)); // Reallocating it to receive its new char*
-                memcpy(array[begin], array[i], size2+1);
-                array[i] = (char*)realloc(array[i], (size1+1) * sizeof(char)); // Reallocating it to receive its new char*
-                memcpy(array[i], data, size1+1);
-                free(data); // Deallocating memory from data, the auxiliar variable
+        for(int i = 0; i < q_items-1; i++){
+            if(string_compare(array[i], array[i+1]) > 0){
+                void *data = array[i];
+                array[i] = array[i+1];
+                array[i+1] = data;
                 swap = true;
             }
         }
-        begin++;
     }
 }
 
 // It compares two char* and returns 1 if str1 one comes before str2 in the alfabet, -1 if it comes after and 0 if they're the same char*
 int string_compare(char *str1, char *str2){
-    int size1 = strlen(str1), size2 = strlen(str2);
-    for(int i = 0; i < size1 and i < size2; i++){
-        if(str1[i] > str2[i])
+    for(; *str1 != 0 and *str2 != 0; str1++, str2++){
+        if(*str1 > *str2)
             return 1;
-        else if(str1[i] < str2[i])
+        else if(*str1 < *str2)
             return -1;
     }
+    int size1 = strlen(str1), size2 = strlen(str2);
     if(size1 > size2)
         return 1;
     else if(size1 < size2)
@@ -127,11 +110,34 @@ int string_compare(char *str1, char *str2){
 
 // Function to realize what was asked in the statement
 void operate(words_t *words, int operation){
-    switch(operation){
-        case 2:
-            bubblesort(words->splitted, words->q_words);
-            break;
-    }
+    if(operation == 2)
+        bubblesort(words->splitted, words->q_words);
     for(int i = 0; i < words->q_words; i++)
         printf("%s\n", words->splitted[i]);
+}
+
+// Works similarly as strtok from string.h library
+char *my_strtok(char *string, const char *delimiter){
+    static char *buffer;
+    if(string != NULL)
+        buffer = string;
+    if(buffer[0] == 0)
+        return NULL;
+    char *return_string = buffer, *copy_buffer;
+    const char *COPY_DELIM;
+    for(copy_buffer = buffer; *copy_buffer != 0; copy_buffer++){
+        for(COPY_DELIM = delimiter; *COPY_DELIM != 0; COPY_DELIM++){
+            if(*copy_buffer == *COPY_DELIM){
+                *copy_buffer = 0;
+                buffer = copy_buffer+1;
+                if(copy_buffer == return_string){
+                    return_string++;
+                    continue;
+                }
+                return return_string;
+            }
+        }
+    }
+    buffer = copy_buffer;
+    return return_string;
 }
