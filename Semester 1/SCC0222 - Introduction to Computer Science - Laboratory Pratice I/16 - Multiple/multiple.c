3c9adecc85f771fb960d425ae5901b320f84e688
Pedro Perez <pedropc@usp.br>
2021-02-04 23:36:13 -0300

    Adding comments to multiple.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/16 - Multiple/multiple.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/16 - Multiple/multiple.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Defining macro or and setting it to the or boolean condition
 #define or ||
 
+// Main function
 int main(void){
     int n_prints, num1, num2;
-    scanf("%d %d %d", &n_prints, &num1, &num2);
-    for(int i = 0, check_multiple = 0; i < n_prints; check_multiple++){
+    scanf("%d %d %d", &n_prints, &num1, &num2); // Reads 3 integers from stdin
+    for(int i = 0, check_multiple = 0; i < n_prints; check_multiple++){ // Prints n_prints multiples from either num1 or num2
         if(check_multiple % num1 == 0 or check_multiple % num2 == 0){
             printf("%d\n", check_multiple);
             i++;
