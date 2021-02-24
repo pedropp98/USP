caffe596c267ff6690ca39f5d480c3dc84458e48
Pedro Perez <pedropc@usp.br>
2021-02-24 14:20:01 -0300

    Updating leapyear.c file, using ternary operator

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/01 - Leap Year/leapyear.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/01 - Leap Year/leapyear.c
@@ -5,9 +5,6 @@
 int main(void){
     int year;
     scanf("%d", &year);
-    if(year % 4 == 0) // If the remainder of year by 4 is 0, then year is a leap year
-        printf("SIM\n");
-    else
-        printf("NAO\n");
+    (year % 4 == 0) ? printf("SIM\n") : printf("NAO\n"); // Ternary operator
     return 0;
 }
