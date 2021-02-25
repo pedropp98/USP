0c2b36a06a69ac29785f003b78323f90794fb903
Pedro Perez <pedropc@usp.br>
2021-02-25 14:17:36 -0300

    Adding comments to last.c file

--- Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/26 - Changing Last Name/last.c
+++ Semester 1/SCC0222 - Introduction to Computer Science - Laboratory Pratice I/26 - Changing Last Name/last.c
@@ -2,14 +2,17 @@
 #include<stdlib.h>
 #include<string.h>
 
+// Defining macros to use in the program
 #define BUFFER 4096
 #define and &&
 
+// Defining a new type names_t to store the names
 typedef struct{
     char **names;
     int qtd_names;
 }names_t;
 
+// Function definition
 char *readline(FILE*);
 char *my_strtok(char*, const char*);
 names_t **input_names(int*);
@@ -17,6 +20,7 @@ void change_last_names(names_t**, int);
 void print_result(names_t**, int);
 void free_memory(names_t**, int);
 
+// Main function
 int main(void){
     char *read_name;
     int i = 0;
@@ -26,6 +30,7 @@ int main(void){
     free_memory(name, i);
 }
 
+// Reads a text line from a FILE* and stores every character in a char*, then returns it as a string
 char *readline(FILE *in){
     char *string = NULL;
     int pos = 0, character;
@@ -67,6 +72,7 @@ char *my_strtok(char *string, const char *delimiter){
     return return_string;
 }
 
+// Reads every name from stdin, then split them in the space character, then stores it in the respective names_t position
 names_t **input_names(int *i){
     int side_i = *i;
     names_t **name = NULL;
@@ -86,6 +92,7 @@ names_t **input_names(int *i){
     return name;
 }
 
+// As requested in the statement, this function changes the last name from the first word with the last name from the last word, and so on
 void change_last_names(names_t **name, int i){
     for(int j = 0, k = i-1; j <= i / 2 and k >= i / 2; j++, k--){
         char *copy = name[j]->names[name[j]->qtd_names-1];
@@ -94,6 +101,7 @@ void change_last_names(names_t **name, int i){
     }
 }
 
+// Prints every name
 void print_result(names_t **name, int i){
     for(int j = 0; j < i; j++){
         for(int k = 0; k < name[j]->qtd_names; k++){
@@ -103,6 +111,7 @@ void print_result(names_t **name, int i){
     }
 }
 
+// Deallocates memory allocated
 void free_memory(names_t **name, int i){
     for(int j = 0; j < i; j++){
         free(name[j]->names[0]);
